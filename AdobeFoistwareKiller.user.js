f8ed455095f87875b3cecefb363f782189cfad02
TakaDaka. (@djtkdk_086969) <takadaka_0toge1overz@yahoo.co.jp>
2016-05-27 10:45:47 +0900

    Initial commit

---
+++ AdobeFoistwareKiller.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @author         たかだか。(TakaDaka.)
+// @name           Adobe Foistware Killer
+// @name:ja        Adobe Foistware Killer
+// @namespace      https://twitter.com/djtkdk_086969
+// @description:ja Adobe製品ダウンロードページの「オプションのプログラム」のチェックを自動的に外します。
+// @description    Automatically unchecks the "Optional Offer" checkbox on download pages of Adobe products.
+// @include        *://get*.adobe.com/*
+// @version        0.26
+// @grant          none
+// @license        MIT License; https://opensource.org/licenses/mit-license.php
+// @compatible     firefox
+// @compatible     chrome
+// ==/UserScript==
+
+(function() {
+    console.log("AFK: Started.");
+    window.addEventListener ("DOMContentLoaded", check_elem());
+
+    var mo =
+        new MutationObserver(function(mutationEventList){
+            check_elem();
+        });
+    var mo_conf = {
+        childList: true,
+        attributes: false,
+        characterData: false,
+        subtree: true
+    };
+    mo.observe(document.querySelector("body"), mo_conf);
+
+    function check_elem() {
+        console.log("AFK: Checking elements...");
+        var offer = document.getElementById("offersInformationPane");
+        if(offer === null) return;
+        var cb = offer.getElementsByTagName("input");
+        if(cb.length > 0) {
+            mo.disconnect();
+            Array.prototype.slice.call(cb).forEach(function (node) {
+                if(node.checked) {
+                    console.log("AFK: Foistware offer detected! Unchecking the #offerCheckbox.");
+                    var event = new Event('change');
+                    node.checked = false;
+                    node.dispatchEvent(event);
+                }
+            });
+            if(offer.querySelectorAll("p.afk_info").length === 0) {
+                var info = document.createElement("p");
+                info.className = "afk_info";
+                if(navigator.language.substr(0,2) === "ja") {
+                    info.innerHTML = "Adobe Foistware Killer によりオプションのプログラムが拒否されました。";
+                } else {
+                    info.innerHTML = "Adobe Foistware Killer declined the optional offer.";
+                }
+                offer.appendChild(info);
+            }
+        }
+    }
+})();
+
