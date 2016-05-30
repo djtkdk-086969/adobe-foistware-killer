a257c3f6e3c2c8e335d7a1b1165db55adcb64dd2
TakaDaka. (@djtkdk_086969) <djtkdk-086969@users.noreply.github.com>
2016-05-30 18:00:46 +0900

    Old versions: Changed namespace

--- old_versions/AdobeFoistwareKiller.user.js
+++ old_versions/AdobeFoistwareKiller.user.js
@@ -1,21 +1,21 @@
 // ==UserScript==
-// @author      たかだか。(TakaDaka.)
-// @name        Adobe Foistware Killer
-// @namespace   http://sotmentd0zaki.myhome.cx/
-// @description Counter the Adobe's "Optional Offer" foistware (for Flash Player and Reader)
-// @include     *://get*.adobe.com/*
-// @version     beta 0.14
-// @grant       none
+// @author         たかだか。(TakaDaka.)
+// @name           Adobe Foistware Killer
+// @namespace      https://twitter.com/djtkdk_086969
+// @description:ja Adobe製品ダウンロードページの「オプションのプログラム」のチェックを自動的に外します。
+// @description    Automatically unchecks the "Optional Offer" checkbox on download pages of Adobe products.
+// @include        *://get*.adobe.com/*
+// @version        0.22
+// @grant          none
+// @license        https://opensource.org/licenses/mit-license.php
 // ==/UserScript==
 
 (function() {
-    //var already_run = false;
     console.log("AFK: Started.");
     window.addEventListener ("DOMContentLoaded", check_elem());
 
     var mo =
         new MutationObserver(function(mutationEventList){
-            //console.log("Mutation observed");
             check_elem();
         });
     var mo_conf = {
@@ -23,31 +23,24 @@
         attributes: false,
         characterData: false,
         subtree: true
-    }
+    };
     mo.observe(document.querySelector("body"), mo_conf);
 
     function check_elem() {
         console.log("AFK: Checking elements...");
-        loc = location.href;
-        if(document.getElementById("offerCheckbox") != null) {
+        if(document.getElementById("offerCheckbox") !== null) {
             mo.disconnect();
             var event = new Event('change');
-            console.log("AFK: AFK has found a foistware offer! Unchecking the offerCheckbox.");
-            document.getElementById("offerCheckbox").checked=false
+            console.log("AFK: Foistware offer detected! Unchecking the #offerCheckbox.");
+            document.getElementById("offerCheckbox").checked=false;
             document.getElementById("offerCheckbox").dispatchEvent(event);
             var info = document.createElement("p");
-            info.innerHTML = "Adobe Foistware Killer によりチェックボックスが解除されました。";
-            document.getElementById("offersInformationPane").appendChild(info);
-
-            console.log("AFK: Redirecting to Distribution page.");
-            /* 無関係なソフトを勧めてきたら */
-
-            if(loc.match("/flashplayer/") != null) {
-                location.href="https://www.adobe.com/jp/products/flashplayer/distribution3.html";
-            } else if (loc.match("/reader/") != null) {
-                location.href="http://get.adobe.com/jp/reader/enterprise/";
+            if(navigator.language.substr(0,2) === "ja") {
+                info.innerHTML = "Adobe Foistware Killer によりオプションのプログラムが拒否されました。";
+            } else {
+                info.innerHTML = "Adobe Foistware Killer declined the optional offer.";
             }
-
+            document.getElementById("offersInformationPane").appendChild(info);
         }
     }
 
